96966d499e41375249f2f5ddc9dbffb70826540d
explosion-mental <explosion0mental@gmail.com>
2021-09-16 19:14:30 -0500

    no, this isn't ISC

--- arg.h
+++ arg.h
@@ -1,26 +1,11 @@
-/*
- * ISC-License
- *
- * Copyright 2017 Laslo Hunhold <dev@frign.de>
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
+/* See LICENSE file for copyright and license details. */
+
 #ifndef ARG_H
 #define ARG_H
 
 extern char *argv0;
 
-/* int main(int argc, char *argv[]) */
+/* use: int main(int argc, char *argv[]) */
 #define ARGBEGIN for (argv0 = *argv, *argv ? (argc--, argv++) : ((void *)0);      \
                       *argv && (*argv)[0] == '-' && (*argv)[1]; argc--, argv++) { \
                  	int i_, argused_;                                         \
