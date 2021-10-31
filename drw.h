7ccd016e5d925009a2662b14cd16ae9adffb6e71
explosion-mental <explosion0mental@gmail.com>
2021-10-31 16:45:51 -0500

    original drw.c and drw.h

--- drw.h
+++ drw.h
@@ -52,7 +52,6 @@ void drw_setscheme(Drw *drw, Clr *scm);
 /* Drawing functions */
 void drw_rect(Drw *drw, int x, int y, unsigned int w, unsigned int h, int filled, int invert);
 int drw_text(Drw *drw, int x, int y, unsigned int w, unsigned int h, unsigned int lpad, const char *text, int invert);
-int drw_text_align(Drw *drw, int x, int y, unsigned int w, unsigned int h, const char *text, int textlen, int align);
 
 /* Map functions */
 void drw_map(Drw *drw, Window win, int x, int y, unsigned int w, unsigned int h);
