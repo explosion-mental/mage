f875e33281ca5b3efef27056b0639a14863b33b2
explosion-mental <explosion0mental@gmail.com>
2021-11-30 22:20:10 -0500

    getting the idea about handling layouts

--- layouts.c
+++ layouts.c
@@ -1,5 +1,6 @@
 //TODO layouts will be added after thumbnail mode is working, then I could
 //deviate from thumbnail mode and make an 'alternative' mode.
+//Something like `resizeclient` with source and destiny position (x,y) and dimestion (w,h)
 void
 monocle(void)
 {
